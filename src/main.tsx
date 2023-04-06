0e881a1b85f765dd4b483d6ce1dc60f12cc5e1ca
Adrien Baudet <baudetad@gmail.com>
2023-04-06 16:14:18 +0200

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import App from "./App";
 import GameViewer from "./GameViewer";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <GameViewer />
-  </React.StrictMode>
+  <GameViewer />
 );
