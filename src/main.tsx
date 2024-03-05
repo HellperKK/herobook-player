a2d5892f3388f67b99054fa6a27431287b533201
HellperKK <baudetad@gmail.com>
2024-03-05 13:33:38 +0100

    changed router

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import GameViewer from "./GameViewer";
 import {
   Route,
-  BrowserRouter,
+  HashRouter,
   Routes
 } from "react-router-dom";
 import PlayerMenu from "./pages/PlayerMenu";
@@ -13,11 +13,11 @@ import Player from "./pages/Player";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PlayerMenu />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
